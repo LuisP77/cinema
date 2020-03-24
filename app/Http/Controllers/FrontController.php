580016e901323f31f6848b42609cea818e9e8ffc
Luis <luis@example.com>
2020-03-24 21:31:52 +0100

    10 - Templates

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    function index () {
+        return view('index');
+    }
+
+    function contact () {
+        return view('contact');
+    }
+
+    function reviews () {
+        return view('reviews');
+    }
+
+    function admin () {
+        return view('admin.index');
+    }
+/*test*/
+}
